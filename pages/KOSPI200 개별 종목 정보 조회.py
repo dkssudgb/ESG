c88760435a98b6bc88964acc3cbd7af2023c0315
Sankamita3131 <sankamita3131@gmail.com>
2022-10-22 23:43:25 +0900

    1

--- pages/KOSPI200 개별 종목 정보 조회.py
+++ pages/KOSPI200 개별 종목 정보 조회.py
@@ -46,8 +46,17 @@ with col2:
     options2 = st.multiselect('COLUMN', options_list_21, options_list_22)
 
 dict_agg = {'일자': "count", "연도": "count", "연도월": "count", '종목코드': "count", '종목명': "count", 
-            '종가': 'mean', '시가': 'mean', '고가': 'mean', '저가': 'mean', '등락률': 'mean', '거래량', 'ESG_종합', 'ESG_환경', 'ESG_사회', 'ESG_지배구조'}
-df = df.groupby(options1)[options2].sum()
+            '종가': 'mean', '시가': 'mean', '고가': 'mean', '저가': 'mean', '등락률': 'mean', '거래량': 'mean', 
+            'ESG_종합': lambda x:x.mode(), 'ESG_환경': lambda x:x.mode(), 'ESG_사회': lambda x:x.mode(), 'ESG_지배구조': lambda x:x.mode()}
+
+def fn(dict):
+    tmp = {}
+    for k, v in dict_agg.items():
+        if k in options2:
+            tmp[k] = v
+    return tmp
+
+df = df.groupby(options1)[options2].aggregate(fn(dict_agg))
 st.dataframe(df)
 
 
@@ -56,3 +65,4 @@ st.dataframe(df)
 # ['일자', '종목코드', '종목명', '종가', '시가', '고가', '저가', '등락률', '거래량', 'ESG_종합',
 #        'ESG_환경', 'ESG_사회', 'ESG_지배구조', 'ESG_종합_num', 'ESG_환경_num',
 #        'ESG_사회_num', 'ESG_지배구조_num', '연도월', '연도']
+
