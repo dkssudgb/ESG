a6b94201c3dd699d921e312990ff24a9ed734e87
Sankamita3131 <sankamita3131@gmail.com>
2022-10-22 20:05:23 +0900

    Update 가설 검증(가제).py

---
+++ pages/가설 검증(가제).py
@@ -0,0 +1,16 @@
+import koreanize_matplotlib
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+import plotly.express as px
+import seaborn as sns
+import streamlit as st
+
+st.set_page_config(
+    page_title="Likelion AI School Midproject Team13",
+    page_icon="📈",
+    layout="wide",
+)
+
+st.markdown("# 대제목")
+st.markdown("## 중제목")
