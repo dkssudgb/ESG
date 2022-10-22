fad0e563a951167462e3aae66c15f58a8a9ac440
Sankamita3131 <sankamita3131@gmail.com>
2022-10-22 21:00:04 +0900

    1

--- pages/KOSPI200 개별 종목 정보 조회.py
+++ pages/KOSPI200 개별 종목 정보 조회.py
@@ -19,28 +19,34 @@ st.set_page_config(
     layout="wide",
 )
 
-file_name = "./data\KOSPI200_ESGrate.gzip"
+file_name = "./data\KOSPI200_ESGrate.csv"
 
 st.markdown("# KOSPI 200 지수와 KOSPI 200 ESG 지수 비교")
 st.markdown("## KOSPI 200 지수와 KOSPI 200 ESG 지수의 데이터프레임")
-df = pd.read_parquet(file_name)
+
 @st.cache(allow_output_mutation=True)
-def load_data(file_path):
-    if 
-    data = pd.read_csv(file_path)
-    return data
-df = load_data(df)
+def load_data(file_path, encoding="utf-8"):
+    file_format = os.path.splitext(file_path)
+    if file_format == "csv":
+        data = pd.read_csv(file_path, encoding=encoding)
+        return data
+    elif file_format == "parquet" or "gzip":
+        df = pd.read_parquet(file_name, engine='python')
+    else:
+        raise Exception("data format err")
+    return df
+df = load_data(file_name)
 
 
 
-col1, col2 = st.columns(2)
+col1, col2, col3 = st.columns(3)
 
 with col1:
     options_list_11 = pd.read_csv(file_name).columns
     options_list_12 = ["일자"]
     options1 = st.multiselect('Index 선택', options_list_11, options_list_12)
 
-with col2:
+with col2, col3:
     options_list_21 = pd.read_csv(file_name).columns
     options_list_22 = ["KOSPI 200 종가", "KOSPI 200 ESG 종가"]
     options2 = st.multiselect('Column 선택', options_list_21, options_list_22)
