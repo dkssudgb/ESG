003010c0a7582418e9e9f448b6055f8a8b64e1cb
dkssudgb <leehb9360@likelion.org>
2023-01-28 05:35:55 +0900

    Update ESG.py

--- ESG.py
+++ ESG.py
@@ -1,6 +1,5 @@
-# streamlit run ESG.py
-
 import streamlit as st
+from PIL import Image
 
 st.set_page_config(
     page_title="Likelion AI School Midproject Team13",
@@ -8,13 +7,20 @@ st.set_page_config(
     layout="wide",
 )
 
-st.markdown('# ESG')
-st.write("ESG는 환경(Environmental), 사회(Social) 그리고 기업 지배구조(corporate Governance)를 말하며 기업이나 비즈니스에 대한 투자의 지속 가능성과 사회에 미치는 영향을 측정하는 세 가지 핵심 요소이다.")
-st.markdown("""reference - [위키백과:환경, 사회, 기업 지배구조](https://ko.wikipedia.org/wiki/%ED%99%98%EA%B2%BD,_%EC%82%AC%ED%9A%8C,_%EA%B8%B0%EC%97%85_%EC%A7%80%EB%B0%B0%EA%B5%AC%EC%A1%B0)""")
+st.title('ESG')
 
-st.text("")
-st.text("")
+img = Image.open("./fig/esg.png")
+st.image(img)
+st.write("이미지 출처: [ESG포털](https://esg.krx.co.kr/contents/01/01010100/ESG01010100.jsp)")
 
-st.markdown('## ESG에 대한 전망')
+st.markdown("")
+st.markdown("""#### ESG는 환경(Environmental), 사회(Social), 기업 지배구조(corporate Governance)를 말하며 
+            \n#### 기업이나 비즈니스에 대한 투자의 지속 가능성과 사회에 미치는 영향을 측정하는 세 가지 핵심 요소이다.""")
+st.markdown("reference - [위키백과:환경, 사회, 기업 지배구조](https://ko.wikipedia.org/wiki/%ED%99%98%EA%B2%BD,_%EC%82%AC%ED%9A%8C,_%EA%B8%B0%EC%97%85_%EC%A7%80%EB%B0%B0%EA%B5%AC%EC%A1%B0)")
+
+st.markdown("")
+st.markdown("")
+
+st.title('ESG에 대한 전망')
 st.write("ESG는 투자자들에게 많은 관심을 받고 있는 추세로 보인다. 이에 따라 회사 경영에서 ESG를 고려할 뿐만 아니라, 많은 연구들에서도 ESG를 통한 투자가 수익률이 뒤떨어지지 않고 투자의 안정성을 고무시키는 것으로 보고 있다.")
 st.markdown("""reference - [박준신, 안재준 and 오경주. (2021). 코로나19 이후 ESG 투자 전략 평가: ESG 인덱스 성과를 중심으로. 지식경영연구, 22(4), 87-101.](http://doi.org/10.15813/kmr.2021.22.4.005)""")
