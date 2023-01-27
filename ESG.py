70874ae1909dd114ec2ccbd10b5971a8c72cf46f
dkssudgb <leehb9360@likelion.org>
2023-01-28 05:36:28 +0900

    Update ESG.py

--- ESG.py
+++ ESG.py
@@ -11,11 +11,12 @@ st.title('ESG')
 
 img = Image.open("./fig/esg.png")
 st.image(img)
-st.write("이미지 출처: [ESG포털](https://esg.krx.co.kr/contents/01/01010100/ESG01010100.jsp)")
 
 st.markdown("")
 st.markdown("""#### ESG는 환경(Environmental), 사회(Social), 기업 지배구조(corporate Governance)를 말하며 
             \n#### 기업이나 비즈니스에 대한 투자의 지속 가능성과 사회에 미치는 영향을 측정하는 세 가지 핵심 요소이다.""")
+
+st.markdown("reference - [ESG포털](https://esg.krx.co.kr/contents/01/01010100/ESG01010100.jsp)")
 st.markdown("reference - [위키백과:환경, 사회, 기업 지배구조](https://ko.wikipedia.org/wiki/%ED%99%98%EA%B2%BD,_%EC%82%AC%ED%9A%8C,_%EA%B8%B0%EC%97%85_%EC%A7%80%EB%B0%B0%EA%B5%AC%EC%A1%B0)")
 
 st.markdown("")
